5b13d5c03ac95647d7bfb69ffb92d7b283b65a9b
Jeff Pace <jeugenepace@gmail.com>
2011-08-02 13:02:14 -0400

    Added main program.

---
+++ spacebar_listener.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/jruby -w
+# -*- ruby -*-
+
+include Java
+
+import java.awt.event.KeyEvent
+import java.awt.event.KeyListener
+
+class KeyList 
+  include KeyListener
+
+  attr_reader :keytime
+
+  def initialize
+    @keytime = nil
+  end
+
+  def clear
+    @keytime = nil
+  end
+
+  def keyTyped e
+    # ignore all after the first input ...    
+    return if @keytime
+
+    keychar = e.get_key_char
+
+    if keychar == KeyEvent::VK_SPACE
+      @keytime = Time.new
+    end
+  end
+
+  def keyPressed e
+  end
+
+  def keyReleased e
+  end
+
+end
