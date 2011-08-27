1c8f85c8cc6ea73b7648bdc92933702fdd4dbdd7
Jeff Pace <jeugenepace@gmail.com>
2011-08-27 16:15:39 -0400

    Refactored testpanel into separate class.

--- mackworth.rb
+++ mackworth.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/jruby -w
 # -*- ruby -*-
 
-require 'csv'
 require 'set'
 require 'pathname'
 
@@ -10,6 +9,7 @@ require 'drawer'
 require 'panel'
 require 'spacebarlistener'
 require 'swingutil'
+require 'testframe'
 
 include Java
 
@@ -63,46 +63,6 @@ class ErraticNumberGenerator
 
 end
 
-
-
-class MackworthTestResultsFile 
-
-  CSV_HEADER_FIELDS = [ "userid", "duration", "answered", "is_long", "accurate" ]
-
-  CSV_FILE_NAME = 'mackworth.csv'
-
-  def self.home_directory
-    home = ENV['HOME']
-    unless home
-      home = (ENV['HOMEDRIVE'] || "") + (ENV['HOMEPATH'] || "")
-    end
-    
-    homedir = ENV['HOME'] || (ENV['HOMEDRIVE'] + ENV['HOMEPATH'])
-    Pathname.new(homedir)
-  end
-
-  def initialize
-    @csv_file = self.class.home_directory + CSV_FILE_NAME
-    
-    @csv_lines = @csv_file.exist? ? CSV.read(@csv_file.to_s) : [ CSV_HEADER_FIELDS ]
-  end
-
-  def addlines lines
-    @csv_lines.concat lines
-  end
-
-  def write
-    @csv_lines.each do |line|
-      puts line
-    end
-
-    CSV.open @csv_file.to_s, 'w' do |csv|
-      @csv_lines.each do |line|
-        csv << line
-      end
-    end
-  end
-end
 
 module MackworthTestConstants
 
@@ -212,7 +172,7 @@ class IntroRenderer < MackworthLineDrawer
 end
 
 
-class OutroRenderer < MackworthLineDrawer
+class OutroRenderer < TextRenderer
 
   attr_reader :text  
 
@@ -354,11 +314,14 @@ class MackworthTest < MackworthTestRunner
     super(mainpanel, MackworthTestConstants::ITERATIONS_PER_TEST)
   end
 
+  CSV_HEADER_FIELDS = [ "userid", "duration", "answered", "is_long", "accurate" ]
+
+  CSV_FILE_NAME = 'mackworth.csv'
+
   def write_responses
-    resfile = MackworthTestResultsFile.new
-
+    resfile = CSVFile.new CSV_FILE_NAME, CSV_HEADER_FIELDS
+    
     resfile.addlines @responses
-
     resfile.write
   end
 
@@ -396,118 +359,38 @@ class MackworthTestIntro
 end
 
 
-class MackworthTestFrame < JFrame
+class MackworthTestFrame < TestFrame
 
   def initialize
-    super MackworthTestConstants::APP_NAME
-
-    menubar = JMenuBar.new
-
-    test_menu = JMenu.new "Test"
-    test_menu.mnemonic = KeyEvent::VK_T
-
-    item_new = JMenuItem.new "New"
-    item_new.mnemonic = KeyEvent::VK_N
-    item_new.tool_tip_text = "Run a new test"
-    
-    item_new.add_action_listener do |e|
-      MackworthTest.new @panel
-      @panel.grab_focus
-    end
-
-    test_menu.add item_new
-
-    item_intro = JMenuItem.new "Intro"
-    item_intro.mnemonic = KeyEvent::VK_I
-    item_intro.tool_tip_text = "Run the intro"
-    
-    item_intro.add_action_listener do |e|
-      MackworthTestIntro.new(@panel)
-      @panel.grab_focus
-    end
-
-    test_menu.add item_intro
-
-    item_demo = JMenuItem.new "Demo"
-    item_demo.mnemonic = KeyEvent::VK_D
-    item_demo.tool_tip_text = "Run the demo"
-    
-    item_demo.add_action_listener do |e|
-      MackworthTestDemo.new(@panel)
-      @panel.grab_focus
-    end
-
-    test_menu.add item_demo
-
-    item_exit = JMenuItem.new "Exit"
-    item_exit.add_action_listener do |e|
-      dialog = javax.swing.JDialog.new
-
-      ok = JOptionPane.show_confirm_dialog self, "Are you sure you want to quit?", "Quit?", JOptionPane::YES_NO_OPTION
-      
-      if ok == 0
-        java.lang.System.exit 0
-      end
-    end
-    
-    item_exit.mnemonic = KeyEvent::VK_X
-    item_exit.tool_tip_text = "Exit application"
-
-    test_menu.add item_exit
-
-    menubar.add test_menu
-
-    help_menu = JMenu.new "Help"
-    help_menu.mnemonic = KeyEvent::VK_H
-
-    item_about = JMenuItem.new "About"
-    item_about.mnemonic = KeyEvent::VK_A
-    item_about.tool_tip_text = "Show information about the program"    
-
-    item_about.add_action_listener do |e|
-      appname = "Mackworth Psychological Vigilance Test"
-      author  = "Jeff Pace (jeugenepace&#64;gmail&#46;com)"
-      website = "http://www.incava.org"
-      github  = "https://github.com/jeugenepace"
-      msg     = "<html>"
-      msg     << appname
-      msg     << "<hr>"
-      msg     << "Written by #{author}" << "<br>"
-      msg     << "&nbsp;&nbsp;&nbsp #{website}" << "<br>"
-      msg     << "&nbsp;&nbsp;&nbsp #{github}" << "<br>"
-      msg     << "</html>"
-      JOptionPane.show_message_dialog self, msg, "About", JOptionPane::OK_OPTION
-    end
-      
-    help_menu.add item_about
-
-    menubar.add help_menu
-
-    set_jmenu_bar menubar  
-
-    # this works fine on Linux with Java 1.6, but not Windows with any version,
-    # or Linux with Java 1.5:
-    # set_extended_state JFrame::MAXIMIZED_BOTH
-    # set_undecorated true
-    
-    set_default_close_operation JFrame::EXIT_ON_CLOSE
-    set_location_relative_to nil
-    get_content_pane.layout = java.awt.BorderLayout.new
-
-    @panel = MainPanel.new(MackworthTestConstants::BACKGROUND_COLOR)
-
-    get_content_pane.add @panel, java.awt.BorderLayout::CENTER
-
-    @panel.layout = nil
-
-    @panel.request_focus_in_window
-
-    pack
-    set_visible true
-
-    move(0, 0)
-    resize Toolkit.default_toolkit.screen_size
+    super(MackworthTestConstants::APP_NAME)
   end
+
+  def run_test
+    MackworthTest.new @panel
+  end
+
+  def run_intro
+    MackworthTestIntro.new @panel
+  end
+
+  def run_demo
+    MackworthTestDemo.new @panel
+  end
+
+  def get_about_text
+    appname = "Mackworth Psychological Vigilance Test"
+    author  = "Jeff Pace (jeugenepace&#64;gmail&#46;com)"
+    website = "http://www.incava.org"
+    github  = "https://github.com/jeugenepace"
+    text    = "<html>"
+    text    << appname
+    text    << "<hr>"
+    text    << "Written by #{author}" << "<br>"
+    text    << "&nbsp;&nbsp;&nbsp #{website}" << "<br>"
+    text    << "&nbsp;&nbsp;&nbsp #{github}" << "<br>"
+    text    << "</html>"
+  end
+  
 end
 
 class MackworthTestMain
